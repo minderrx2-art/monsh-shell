94bf30dd51af768287d303057efe9db5f85d50dd
Minvydas <minderrx2@gmail.com>
2026-07-02 19:54:40 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,25 +2,17 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-
 	for {
-		select {
-		case <-sigChan:
+		var command string
+		// TODO: Uncomment the code below to pass the first stage
+		fmt.Print("$ ")
+		fmt.Scan(&command)
+		if command == "exit" {
 			return
-		default:
-			var command string
-			// TODO: Uncomment the code below to pass the first stage
-			fmt.Print("$ ")
-			fmt.Scan(&command)
-			fmt.Printf("%s: command not found\n", command)
 		}
+		fmt.Printf("%s: command not found\n", command)
 	}
 }
