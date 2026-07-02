02fc7fafed86b1046764964a4b5a7762490d3fc7
Minvydas <minderrx2@gmail.com>
2026-07-02 19:42:17 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,6 +5,9 @@ import (
 )
 
 func main() {
+	var command string
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+	fmt.Scan(&command)
+	fmt.Printf("%s: command not found\n", command)
 }
