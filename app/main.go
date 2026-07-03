7b5e5c7bb1be6de22f6186ab955fa8f81993371c
Minvydas <minderrx2@gmail.com>
2026-07-03 08:39:08 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,22 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
 func main() {
+	commands := []string{"exit", "echo", "type"}
 	for {
 		reader := bufio.NewReader(os.Stdout)
-		// TODO: Uncomment the code below to pass the first stage
 		fmt.Print("$ ")
 		line, _ := reader.ReadString('\n')
 		lines := strings.Split(strings.TrimSpace(line), " ")
 		command, rest := lines[0], lines[1:]
 		if command == "exit" {
 			return
-		}
-		if command == "echo" {
+		} else if command == "echo" {
 			fmt.Printf("%s\n", strings.Join(rest, " "))
+		} else if command == "type" {
+			if bool := slices.Contains(commands, lines[1]); bool == true {
+				fmt.Printf("%s is a shell builtin\n", lines[1])
+			} else {
+				fmt.Printf("%s: command not found\n", lines[1])
+			}
 		} else {
 			fmt.Printf("%s: command not found\n", command)
 		}
