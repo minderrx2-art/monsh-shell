3e123d48ecc8971f44dcbd7e7ef60805ba01ba3e
Minvydas <minderrx2@gmail.com>
2026-07-03 19:35:30 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"slices"
 	"strings"
 )
@@ -15,19 +16,19 @@ func main() {
 		fmt.Print("$ ")
 		line, _ := reader.ReadString('\n')
 		lines := strings.Split(strings.TrimSpace(line), " ")
-		command, rest := lines[0], lines[1:]
+		command, second_command, rest := lines[0], lines[1], lines[2:]
 
 		if command == "exit" {
 			return
 		} else if command == "echo" {
 			fmt.Printf("%s\n", strings.Join(rest, " "))
 		} else if command == "type" {
-			if contains := slices.Contains(commands, lines[1]); contains == true {
-				fmt.Printf("%s is a shell builtin\n", lines[1])
-			} else if exists, path, err := readBinPath(lines[1]); exists == true && err == nil {
-				fmt.Printf("%s is %s\n", lines[1], path)
+			if contains := slices.Contains(commands, second_command); contains == true {
+				fmt.Printf("%s is a shell builtin\n", second_command)
+			} else if exists, path, err := findPath(second_command); exists == true && err == nil {
+				fmt.Printf("%s is %s\n", second_command, path)
 			} else {
-				fmt.Printf("%s: not found\n", lines[1])
+				fmt.Printf("%s: not found\n", second_command)
 			}
 		} else {
 			fmt.Printf("%s: command not found\n", command)
@@ -35,40 +36,10 @@ func main() {
 	}
 }
 
-func fileExists(path string) bool {
-	_, err := os.Stat(path)
-
-	if err == os.ErrNotExist {
-		return false
-	}
-	return true
-}
-
-func isExecutable(path string) bool {
-	info, err := os.Stat(path)
+func findPath(binary string) (bool, string, error) {
+	path, err := exec.LookPath(binary)
 	if err != nil {
-		return false
+		return false, "", fmt.Errorf("Not found")
 	}
-	mode := info.Mode()
-	if mode.IsRegular() {
-		return false
-	}
-	isUserExecutable := mode&0100 != 0
-	return isUserExecutable
-}
-
-func readBinPath(binary string) (bool, string, error) {
-	paths := strings.Split(os.Getenv("PATH"), ":")
-	filtered_paths := slices.DeleteFunc(paths, func(path string) bool {
-		if strings.Contains(path, binary) {
-			return false
-		}
-		return true
-	})
-	for _, f_path := range filtered_paths {
-		if fileExists(f_path) && isExecutable(f_path) {
-			return true, f_path, nil
-		}
-	}
-	return false, "", fmt.Errorf("Binary has no PATH")
+	return true, path, nil
 }
