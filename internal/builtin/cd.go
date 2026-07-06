04a5ae2084f5a3c0a1afe5ebe494e1cc1e3f31ce
Minvydas <minderrx2@gmail.com>
2026-07-06 17:40:03 +0100

    codecrafters submit [skip ci]

--- internal/builtin/cd.go
+++ internal/builtin/cd.go
@@ -6,9 +6,17 @@ import (
 )
 
 func Cd(dir string) error {
-	err := os.Chdir(dir)
-	if err != nil && os.IsNotExist(err) {
-		return fmt.Errorf("cd: %s: No such file or directory", dir)
+	if dir == "~" {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return fmt.Errorf("cd: %s: No such file or directory", dir)
+		}
+		os.Chdir(homeDir)
+	} else {
+		err := os.Chdir(dir)
+		if err != nil && os.IsNotExist(err) {
+			return fmt.Errorf("cd: %s: No such file or directory", dir)
+		}
 	}
 	return nil
 }
