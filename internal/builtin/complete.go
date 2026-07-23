1909edc7f70bf43a6dc93833647a91031f71f9be
Minvydas <minderrx2@gmail.com>
2026-07-23 21:35:26 +0100

    codecrafters submit [skip ci]

--- internal/builtin/complete.go
+++ internal/builtin/complete.go
@@ -8,5 +8,7 @@ func Complete(args []string) {
 	switch args[0] {
 	case "-p":
 		fmt.Printf("complete: %s: no completion specification", args[1])
+	default:
+		fmt.Printf("complete: %s: no completion specification", args[1])
 	}
 }
